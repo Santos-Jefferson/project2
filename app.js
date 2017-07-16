f17cf298f03d680f5412f924507e0e72ecd7c047
Santos-Jefferson <jefferson2004@gmail.com>
2017-07-16 00:15:50 -0300

    save

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
 }))
 
 //connecting postgres
-client.connect();
+//client.connect();
 
 // Because we will be using post values, we need to include the body parser module
 var bodyParser = require('body-parser')
