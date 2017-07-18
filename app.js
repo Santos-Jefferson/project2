9cc95a2adde5cb3e9e3f3d9389cc37c0e31af326
Santos-Jefferson <jefferson2004@gmail.com>
2017-07-18 11:13:38 -0300

    con

--- app.js
+++ app.js
@@ -213,6 +213,7 @@ app.get('/', function(req, res){
 //Users
 //get all users
 var pg = require('pg');
+var cnx = 'postgres://avhmfsjekhngsy:6052c2d54b48ab1a7e55c9ae678af9e25dbc7877c77cb13cbb5a23956d5d1b4c@ec2-54-197-232-155.compute-1.amazonaws.com:5432/dfv5tacs6f0k53';
 //var cnx = 'postgres://postgres:aaml0509@localhost:5432/familyBudget';
 
 app.get('/getdata', function(req, res, next) {
